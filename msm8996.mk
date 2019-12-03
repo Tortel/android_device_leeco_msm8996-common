a08131daab3a7db1c2b593f705c3014c0a1f332a
Scott Warner <Tortel1210@gmail.com>
2019-12-03 14:43:55 -0500

    Revert "msm8996: Create system copy of ims-ext-common and add to classpath"
    This reverts commit b18aa8f889f104115970f24feb8fb603d484d550.

--- msm8996.mk
+++ msm8996.mk
@@ -351,11 +351,10 @@ PRODUCT_SOONG_NAMESPACES += \
     device/leeco/msm8996-common
 
 PRODUCT_PACKAGES += \
-    ims-ext-common_system \
+    ims-ext-common \
     telephony-ext
 
 PRODUCT_BOOT_JARS += \
-    ims-ext-common_system \
     telephony-ext
 
 # Seccomp policy
